0ffc509be82c603e5c824cde8e31faef3ba5cc48
αнмєяя кнαиzz <ahmerrkhanzz@gmail.com>
2016-08-23 12:13:55 +0400

    All Functionality done on all pages

--- client/pages/home.controller.js
+++ client/pages/home.controller.js
@@ -8,9 +8,33 @@
         function HomeController($http, sessionService) {
             var vm = this;
             vm.sessionObj = sessionService.getSession();
+
+            function ratePercent(array) {
+                var result = {};
+                var final = {};
+                array.forEach(function (item, idx) {
+                    if (result[item] >= 0) {
+                        result[item]++;
+                    } else {
+                        result[item] = 1;
+                    }
+                });
+                for (var prop in result) {
+                    final[prop] = (result[prop] / array.length) * 100;
+                }
+                return final;
+            }
+
             $http.get('http://localhost:3009/videos?sessionId=' + vm.sessionObj.sessionId).then(function(res){
                 vm.videosList = res.data.data;
                 console.log(vm.videosList);
+                vm.videosList.forEach(function(item){
+                    vm.ratings = item.ratings;
+                    vm.rate = ratePercent(vm.ratings)
+                });
+                
             });
+
+            vm.ratePercent = ratePercent;
         }
 })();
--- client/pages/video-detail.controller.js
+++ client/pages/video-detail.controller.js
@@ -1,93 +1,64 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('application')
         .controller('VideoDetail', VideoDetail);
 
-        function VideoDetail ($http, sessionService, $stateParams) {
-            var vm = this;
-            vm.sessionObj = sessionService.getSession();
+    function VideoDetail($http, sessionService, $stateParams) {
+        var vm = this;
+        vm.sessionObj = sessionService.getSession();
+        vm.rating = [1, 2, 3, 4, 5];
 
-            // function definition
+        // function definition
 
-            
-
-            function percentCalc (array) {
-                // var arr = [4,4,2,1,5,5,5,5,1,3];
-                var result = {};
-                var resultPercentage = [];
-                    array.forEach(function(item, idx){
-                    if (result[item] >= 0) {
-                        result[item]++;
-                    } else {
-                        result[item] = 1;
-                    }
-                    // return result[item];
-                });
-                 console.log(result);
-    
-                for (var prop in result) {
-                    var x = (result[prop] / array.length) * 100;
-                    resultPercentage.push(x);
-                    console.log(resultPercentage);
+        /**
+         * Calculate the rating percentage of the video
+         * @param  {string} array     Count of all current rating of video 
+         * @return {obj}   final      returns the calculated percentages of the available ratings of video
+         */
+        function ratePercent(array) {
+            var result = {};
+            var final = {};
+            array.forEach(function (item, idx) {
+                if (result[item] >= 0) {
+                    result[item]++;
+                } else {
+                    result[item] = 1;
                 }
-                
-                
+            });
+            for (var prop in result) {
+                final[prop] = (result[prop] / array.length) * 100;
             }
+            return final;
+        }
 
-
-
-            // function countInArray(array, value) {
-            //     var count = 0;
-            //     var arr = [];
-            //     for (var i = 0; i <= array.length; i++) {
-            //         if (array[i] === value) {
-            //             count++;
-            //             console.log({
-            //                 value:value,
-            //                 count:count
-            //             });
-            //         }
-            //     }
-            //     var countPercent = (count / array.length) * 100;
-            //     return countPercent;
-            // }
-
-            // function isRating(x) {
-            //     var n = x + 1;
-            //     vm.ratingArray.push(n);
-            //     var a = countInArray(vm.ratingArray, 1);
-            //     var b = countInArray(vm.ratingArray, 2);
-            //     var c = countInArray(vm.ratingArray, 3);
-            //     var d = countInArray(vm.ratingArray, 4);
-            //     var e = countInArray(vm.ratingArray, 5);
-            //     vm.ratePercentage = [a, b, c, d, e];
-            //     console.log("Percentage after i have rated " + vm.ratePercentage);
-            // }
-
-            $http.get('http://localhost:3009/video?sessionId=' + vm.sessionObj.sessionId + '&videoId=' + $stateParams.vid).then(function(res){
-                vm.videoSpecs = res.data.data;
-                vm.ratingArray = vm.videoSpecs.ratings;
-                // vm.percentCalc(vm.ratingArray);
-                // vm.countInArray(vm.ratingArray, 5);
-                console.log(vm.ratingArray);
-                console.log(vm.percentCalc(vm.ratingArray));
-                // var a = countInArray(vm.ratingArray, 1); // returns number of times that element occur in array
-                // var b = countInArray(vm.ratingArray, 2);
-                // var c = countInArray(vm.ratingArray, 3);
-                // var d = countInArray(vm.ratingArray, 4);
-                // var e = countInArray(vm.ratingArray, 5);
-                // vm.ratePercentage = [a, b, c, d, e];
-                // console.log("Percentage before i have rated " + vm.ratePercentage);
-                // vm.rating = [1,2,3,4,5];
+        /**
+         * Calculate my rating percentage of the video
+         * @param  {string} idx     Takes my rating when i click on some value
+         */
+        function myRating(idx) {
+            var count = idx + 1;
+            vm.selected = idx;
+            $http.post('http://localhost:3009/video/ratings?sessionId=' + vm.sessionObj.sessionId, { "videoId": $stateParams.vid, "rating": count }).then(function (res) {
+                var response = res.data.data;
+                vm.newRatingArray = response.ratings;
+                vm.rate = vm.ratePercent(vm.newRatingArray);
             });
 
-
-            // function assignment
-            // vm.isRating = isRating;
-            vm.percentCalc = percentCalc;
-                // vm.countInArray = countInArray;
-            
         }
+
+        $http.get('http://localhost:3009/video?sessionId=' + vm.sessionObj.sessionId + '&videoId=' + $stateParams.vid).then(function (res) {
+            vm.videoSpecs = res.data.data;
+            vm.ratingArray = vm.videoSpecs.ratings;
+            vm.rate = vm.ratePercent(vm.ratingArray);
+            console.log(vm.videoSpecs);
+        });
+
+
+        // function assignment
+        vm.myRating = myRating;
+        vm.ratePercent = ratePercent;
+
+    }
 })();
